004dbba0e638275598255f20dc9b3d59d7528605
andy <643922878@qq.com>
2018-07-31 17:56:25 +0800

    cloud 消息总线

---
+++ cnspringcloudconfig/src/main/java/springcloudconfig/App.java
@@ -0,0 +1,21 @@
+package springcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+}
